96b208253e7c2ddc7a6e261fe0af59646a219900
bayron.meneses <bayron.meneses.registel@gmail.com>
2025-03-25 17:23:59 -0500

    Creacion de pagina nosotros

---
+++ app/(client)/nosotros/page.tsx
@@ -0,0 +1,14 @@
+import { Empleados } from '@/components/nosotros/empleados/Empleados'
+import { Historia } from '@/components/nosotros/historia/Historia'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+        <Historia/>
+        <Empleados/>
+    </>
+  )
+}
+
+export default page
--- components/navar/navar.tsx
+++ components/navar/navar.tsx
@@ -2,8 +2,14 @@
 import Image from "next/image";
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 import logo from "./assets/logo.png";
+import logo2 from "./assets/logo2.png";
+import buscar from "./assets/buscar.png";
+
 export const Navar = () => {
+  const page = usePathname()
   const [scrollPosition, setScrollPosition] = useState({ x: 0, y: 0 });
   const handleScroll = () => {
     setScrollPosition({
@@ -20,19 +26,19 @@ export const Navar = () => {
   },[])
   console.log(scrollPosition)
   return (
-    <nav className={`transition duration-150 ${scrollPosition.y > 600 ? 'bg-[#1f508f]/60 backdrop-blur-md' : null } fixed w-full flex flex-row py-2 gap-5 px-20 z-[99999] `}>
-      <Image src={logo} alt="Logo Registel" width={100} height={25} />
-      <ul className="w-full flex flex-row gap-5 justify-start border-b-2 border-white p-0 items-center">
-        <li className="text-white font-semibold">
+    <nav className={`transition duration-150 ${page === "/" ? scrollPosition.y > 600 ? 'bg-[#1f508f]/60 backdrop-blur-md' : null : "backdrop-blur-xl" } fixed w-full flex flex-row py-2 gap-5 px-20 z-[99999] `}>
+      <Image src={page === "/" ? logo : logo2} alt="Logo Registel" width={100} height={25} />
+      <ul className={`w-full flex flex-row gap-5 justify-start border-b-2 ${page === "/" ? "border-white" : "border-[#1f508f]" } p-0 items-center`}>
+        <li className={`${page === "/" ? "text-white " : "text-[#1f508f]"} font-semibold`}>
            <Link href={'/'}>Inicio</Link> 
         </li>
-        <li className="text-white font-semibold">
+        <li className={`${page === "/" ? "text-white " : "text-[#1f508f]"} font-semibold`}>
             <Link href={'/nosotros'}>Nosotros</Link> 
         </li>
-        <li className="text-white font-semibold">
+        <li className={`${page === "/" ? "text-white " : "text-[#1f508f]"} font-semibold`}>
             <Link href={'/productos&servicios'}>Productos y Servicios</Link>
         </li>
-        <li className="text-white font-semibold">
+        <li className={`${page === "/" ? "text-white " : "text-[#1f508f]"} font-semibold`}>
             <Link href={'/contactenos'}>Contactenos</Link>
         </li>
       </ul>
@@ -40,11 +46,11 @@ export const Navar = () => {
         <form action="" className="w-full flex flex-row justify-end gap-5 items-center ">
             <div className="relative ">
                 <div className="absolute ml-2 mt-1">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" className="w-5 h-5">
-                        <path fillRule="evenodd" d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" clipRule="evenodd" />
-                    </svg>
+                    <Image src={buscar} alt="Imagen de lupa para input de busqueda" width={20} height={20}/>
                 </div>
-                <input type="text" className="py-2 w-40 px-10 bg-transparent border border-white h-7 rounded-xl placeholder-white text-sm" placeholder="Buscar" />
+                <input type="text" 
+                className={`py-2 w-40 px-10 bg-transparent border ${page === "/" ? "border-white" : "border-[#1f508f]" } h-7 rounded-xl placeholder-white text-sm`}
+                 placeholder="Buscar" />
             </div>
         </form>
       </div>
